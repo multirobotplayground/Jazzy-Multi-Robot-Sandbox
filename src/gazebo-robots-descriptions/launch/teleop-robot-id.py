f9870c657e46ce5e71a391ec203376018581c1f8
multirobotplayground <multirobotplayground@gmail.com>
2024-06-04 18:31:36 -0300

    Add more complex worlds

--- src/gazebo-robots-descriptions/launch/teleop-robot-id.py
+++ src/gazebo-robots-descriptions/launch/teleop-robot-id.py
@@ -1,15 +1,15 @@
-import sys
-import os
+#import sys
+#import os
 
-from ament_index_python.packages import get_package_share_directory
+#from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 
 from launch_ros.actions import Node as create_node_description
-from launch.launch_description_sources import PythonLaunchDescriptionSource as load_python_launch_file
-from launch.actions import IncludeLaunchDescription as include_another_launch_file 
+#from launch.launch_description_sources import PythonLaunchDescriptionSource as load_python_launch_file
+#from launch.actions import IncludeLaunchDescription as include_another_launch_file 
 from launch.substitutions import LaunchConfiguration as get_set_argument_val
-from launch.substitutions import PathJoinSubstitution as concatenate_path_strings
-from launch.substitutions import TextSubstitution as replace_string
+#from launch.substitutions import PathJoinSubstitution as concatenate_path_strings
+#from launch.substitutions import TextSubstitution as replace_string
 from launch.actions import DeclareLaunchArgument as create_input_argument
 
 def generate_launch_description(): 
