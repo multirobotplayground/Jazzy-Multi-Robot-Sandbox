cc65171f368dbb4aec28f38452f4835c025795c8
multirobotplayground <multirobotplayground@gmail.com>
2024-06-06 16:43:42 -0300

    Add launch file folder
    In this commit I've added the launch folder again. For some reason
    it was removed from the repo.

---
+++ src/multi-robot-simulations/launch/keyboard_teleop_robot_id_launch.py
@@ -0,0 +1,30 @@
+#import sys
+#import os
+
+#from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+
+from launch_ros.actions import Node as create_node_description
+#from launch.launch_description_sources import PythonLaunchDescriptionSource as load_python_launch_file
+#from launch.actions import IncludeLaunchDescription as include_another_launch_file 
+from launch.substitutions import LaunchConfiguration as get_set_argument_val
+#from launch.substitutions import PathJoinSubstitution as concatenate_path_strings
+#from launch.substitutions import TextSubstitution as replace_string
+from launch.actions import DeclareLaunchArgument as create_input_argument
+
+def generate_launch_description(): 
+    robot_namespace = create_input_argument('namespace', default_value='')
+    get_set_argument_val('namespace')
+
+    teleop_node = create_node_description(
+        package="teleop_twist_keyboard",
+        executable="teleop_twist_keyboard",
+        name="teleop_twist_keyboard",
+        namespace=get_set_argument_val('namespace'),
+        # remappings=[('cmd_vel', concatenate_path_strings([get_set_argument_val('namespace'), "cmd_vel"]))],
+        prefix = 'xterm -e',
+        output='screen'
+    )
+
+    return LaunchDescription([robot_namespace, teleop_node])
+
